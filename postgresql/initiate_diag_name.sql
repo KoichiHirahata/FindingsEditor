32ec142ec83b493235e558d446f045a9d7693c7b
Koichi Hirahata <koichi@hirahata.com>
2017-02-14 07:50:55 +0900

    Improved sql files

--- postgresql/initiate_diag_name.sql
+++ postgresql/initiate_diag_name.sql
@@ -10,588 +10,235 @@ insert into diag_name(
     , '正常'
     , 0
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     1010
     , 'Larynx polyp'
     , '喉頭ポリープ'
     , 1010
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     1020
     , 'Larynx cancer'
     , '喉頭癌'
     , 1020
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     1030
     , 'Leukoplakia'
     , '白斑症'
     , 1030
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     1040
     , 'Larynx inflammation'
     , '喉頭炎'
     , 1040
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     1050
     , 'Hypopharynx cancer'
     , '下咽頭癌'
     , 1050
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     1060
     , 'Vocal cord cyst'
     , '声帯嚢胞'
     , 1060
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2010
     , 'Reflux esophagitis'
     , '逆流性食道炎'
     , 2010
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2020
     , 'GERD(LA grade M)'
     , 'GERD(LA grade M)'
     , 2020
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2030
     , 'GERD(LA grade A)'
     , 'GERD(LA grade A)'
     , 2030
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2040
     , 'GERD(LA grade B)'
     , 'GERD(LA grade B)'
     , 2040
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2050
     , 'GERD(LA grade C)'
     , 'GERD(LA grade C)'
     , 2050
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2060
     , 'GERD(LA grade D)'
     , 'GERD(LA grade D)'
     , 2060
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2070
     , 'Barrett\'s esophagus'
     , 'バレット食道'
     , 2070
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2080
     , 'Unexplained esophageal bleeding'
     , '食道の原因不明の出血'
     , 2080
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2090
     , 'Esophageal ulcer'
     , '食道潰瘍'
     , 2090
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2100
     , 'Esophageal scar'
     , '食道瘢痕'
     , 2100
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2110
     , 'Esophageal ulcer due to treatment'
     , '食道治療後潰瘍'
     , 2110
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2120
     , 'Esophageal scar due to treatment'
     , '食道治療後瘢痕'
     , 2120
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2130
     , 'Mallory-Weiss syndrome'
     , 'Mallory-Weiss syndrome'
     , 2130
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2140
     , 'Esophageal erosion'
     , '食道びらん'
     , 2140
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2150
     , 'Non-reflux esophagitis'
     , '非逆流性食道炎'
     , 2150
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2160
     , 'Esophageal candidiasis'
     , 'カンジダ性食道炎'
     , 21960
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2170
     , 'Cytomegalovirus esophagitis'
     , 'サイトメガロウイルス食道炎'
     , 2170
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2180
     , 'Herpes esophagitis'
     , 'ヘルペスウイルス食道炎'
     , 2180
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2190
     , 'Corrosive esophagitis'
     , '腐蝕性食道炎'
     , 2190
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2200
     , 'Crohn\'s disease of the esophagus'
     , '食道クローン病'
     , 2200
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     2210
     , 'Systemic scleroderma'
     , '全身性強皮症'
     , 2210
     , true
-    )
-    ;
-
-
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3010
     , 'Esophageal polyp'
     , '食道ポリープ'
     , 3010
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3020
     , 'Esophageal papilloma'
     , '食道乳頭腫'
     , 3020
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3030
     , 'Esophageal dysplasia'
     , '食道dysplasia'
     , 3030
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3040
     , 'Superficial esophageal cancer'
     , '食道表在癌'
     , 3040
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3050
     , 'Intramural metastatic carcinoma of esophagus'
     , '壁内転移性癌'
     , 3050
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3060
     , 'Advanced esophageal cancer'
     , '食道進行癌'
     , 3060
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3070
     , 'Remaining lesion after mucosal resection'
     , '食道粘膜切除後癌遺残'
     , 3070
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3080
     , 'Recurrence of esphageal cancer after mucosal resection'
     , '食道粘膜切除後再発癌'
     , 3080
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3090
     , 'Metastatic esophageal cancer'
     , '転移性食道癌'
     , 3090
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3100
     , 'Malignant melanoma of esophagus'
     , '食道悪性黒色腫'
     , 3100
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3110
     , 'Submucosal tumor of esophagus'
     , '食道粘膜下腫瘍'
     , 3110
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     3120
     , 'Malignant lymphoma of esophagus'
     , '食道悪性リンパ腫'
@@ -614,256 +261,103 @@ insert into diag_name(
     , '食道壁外性圧迫'
     , 4010
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4020
     , 'Hiatus hernia'
     , '食道裂孔ヘルニア'
     , 4020
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4030
     , 'Foreign body of esophagus'
     , '食道異物'
     , 4030
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4040
     , 'Diverticulum of esophagus'
     , '食道憩室'
     , 4040
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4050
     , 'Esophageal varices'
     , '食道静脈瘤'
     , 4050
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4060
     , 'Ulcer after treatment of esophageal varices'
     , '静脈瘤治療後潰瘍'
     , 4060
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4070
     , 'Scar after treatment of esophageal varices'
     , '静脈瘤治療後瘢痕'
     , 4070
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4080
     , 'Glycogenic acanthosis of esophagus'
     , 'グリコーゲン・アカントーシス'
     , 4080
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4090
     , 'Ectopic gastric mucosa of esophagus'
     , '食道異所性胃粘膜'
     , 4090
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4100
     , 'Esophageal melanosis'
     , '食道メラノーシス'
     , 4100
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4110
     , 'Esophageal hematoma'
     , '食道血腫'
     , 4110
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4120
     , 'Postoperative esophagus'
     , '食道術後'
     , 4120
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4130
     , 'After total esophagectomy'
     , '食道全摘後'
     , 4130
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4140
     , 'Benign esophageal stricture'
     , '食道良性狭窄'
     , 4140
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4150
     , 'Esophageal web'
     , '食道web'
     , 4150
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4160
     , 'Esophageal achalasia'
     , '食道アカラシア'
     , 4160
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4170
     , 'Esophageal fistula'
     , '食道瘻孔'
     , 4170
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     4180
     , 'Boerhaave Syndrome'
     , '特発性食道破裂（Boerhaave症候群）'
@@ -886,391 +380,157 @@ insert into diag_name(
     , '胃粘膜萎縮（萎縮性胃炎）'
     , 5010
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5020
     , 'Erosive gastritis'
     , 'びらん性胃炎'
     , 5020
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5030
     , 'Erythematous gastritis'
     , '発赤胃炎'
     , 5030
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5040
     , 'Hypertrophic gastritis'
     , '肥厚性胃炎'
     , 5040
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5050
     , 'Hemorrhagic gastritis'
     , '出血性胃炎'
     , 5050
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5060
     , 'Gastropathy-portal hypertensive'
     , '胃病変-門脈圧亢進性'
     , 5060
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5070
     , 'Eosinophilic gastritis'
     , '好酸球性胃炎'
     , 5070
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5080
     , 'Gastric bleeding of unknown origin'
     , '胃の原因不明の出血'
     , 5080
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5090
     , 'Gastric ulcer'
     , '胃潰瘍'
     , 5090
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5100
     , 'Gastric ulcer scar'
     , '胃潰瘍瘢痕'
     , 5100
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5110
     , 'Hemorrhagic gastric ulcer'
     , '出血性胃潰瘍'
     , 5110
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5120
     , 'Dieulafoy lesion'
     , 'デュラフォア病変'
     , 5120
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5130
     , 'Gastropathy-NSAID-related'
     , '胃病変-NSAID 起因性'
     , 5130
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5140
     , 'Acute gastric mucosal lesion(AGML)'
     , '急性胃粘膜病変(AGML)'
     , 5140
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5150
     , 'Acute gastroduodenal mucosal lesion'
     , '急性胃十二指腸粘膜病変'
     , 5150
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5160
     , 'Gastric ulcer-anastomotic'
     , '胃潰瘍-吻合部'
     , 5160
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5170
     , 'Gastric ulcer-postprocedure'
     , '胃治療後潰瘍'
     , 5170
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5180
     , 'Gastric scar-postprocedure'
     , '胃治療後瘢痕'
     , 5180
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5190
     , 'Gastric amyloidosis'
     , '胃アミロイドーシス'
     , 5190
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5200
     , 'Gastric sarcoidosis'
     , '胃サルコイドーシス'
     , 5200
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5210
     , 'Gastric syphilis'
     , '胃梅毒'
     , 5210
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     5220
     , 'Menetrier\'s disease'
     , 'Menetrier病'
     , 5220
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     
     , ''
     , ''
     , 
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     
     , ''
     , ''
     , 
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     
     , ''
     , ''
     , 
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     
     , ''
     , ''
     , 
     , true
-    )
-    ;
-
-insert into diag_name(
-    no
-    , name_eng
-    , name_jp
-    , diag_order
-    , diag_visible)
-    values(
+    ) , (
     
     , ''
     , ''
--- postgresql/sql.md
+++ postgresql/sql.md
@@ -4,10 +4,11 @@
 1. db_version.sql
 1. diag_category.sql
 1. initiate_db.sql
+1. initiate_diag_name.sql
+1. [japanese | english]/initiate_default_findings.sql
 1. [japanese | english]/initiate_department.sql
 1. [japanese | english]/initiate_op_category.sql
 1. initiate_operator.sql
-1. [japanese | english]/initiate_default_findings.sql
 
 ## How to run sql files
 
