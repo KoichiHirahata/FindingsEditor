3db47ac73e433e561756ab998f832611d1475e0f
Koichi Hirahata <koichi@hirahata.com>
2017-07-19 10:04:17 +0900

    Updated version information

--- windows/FindingsEditor/Version.Designer.cs
+++ windows/FindingsEditor/Version.Designer.cs
@@ -42,7 +42,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(221, 19);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Findings Editor Ver. 1.25.1";
+            this.label1.Text = "Findings Editor Ver. 1.25.2";
             // 
             // label2
             // 
